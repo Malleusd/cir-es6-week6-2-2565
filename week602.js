4e81b1330ea194eb60e4030afa539ada030ed350
Malleusd <malleusdraconia63@gmail.com>
2022-11-21 09:56:50 +0700

    fon

---
+++ week602.js
@@ -0,0 +1,4 @@
+let i = 4
+for (i ; i > 1; i=i-1) {
+    console.log("2 x" + i + "=",2*i)
+}
---
+++ week604.js
@@ -0,0 +1,5 @@
+let province = { p: "Trat", p: "Chantaburi" }
+
+for(let thai in province) {
+    console.log(province[thai])
+}
